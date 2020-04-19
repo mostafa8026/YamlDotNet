25881b8fa25dfcbc29a1d684baa4a4453299f802
Antoine Aubry <antoine@gothelist.com>
2020-04-19 19:42:15 +0100

    Fix compilation warning

--- PerformanceTests/YamlDotNet.PerformanceTests.v8.0.0/ReceiptTest.cs
+++ PerformanceTests/YamlDotNet.PerformanceTests.v8.0.0/ReceiptTest.cs
@@ -34,7 +34,7 @@ namespace YamlDotNet.PerformanceTests.v8_0_0
         private readonly StringWriter _buffer = new StringWriter();
 
         private readonly ISerializer _serializer = new SerializerBuilder()
-            .WithNamingConvention(new CamelCaseNamingConvention())
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
             .Build();
 
         [Benchmark(Description = "Serialize v8.0.0")]
